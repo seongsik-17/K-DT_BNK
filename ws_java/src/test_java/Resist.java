126311af98a2e692214d964c0928025fa9f34330
seongsik-17 <mss9810@naver.com>
2025-04-10 17:52:40 +0900

    2025-04-10(java)

--- ws_java/src/test_java/Resist.java
+++ ws_java/src/test_java/Resist.java
@@ -1,27 +1,26 @@
-package test_java;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-public class Resist {
-	Scanner sc = new Scanner(System.in);
-	
-	Map<String, String> map = new HashMap<>();
-
-	while(true)
-	{
-		System.out.print("Key를 입력해주세요>> ");
-		String input_k = sc.nextLine();
-		System.out.print("Value를 입력해주세요");
-		String input_v = sc.nextLine();
-		map.put(input_k, input_v);
-		System.out.println("계속 입력하시겠습니까? 1.예 2.아니오");
-		int onoff = sc.nextInt();
-		if (onoff < 0) {
-			System.out.println("입력을 종료합니다...");
-			break;
-		}
-	}
-
-}
+//package test_java;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.Scanner;
+//
+//public class Resist {
+//	//Scanner sc = new Scanner(System.in);
+//	//Map<String, String> map = new HashMap<>();
+//	
+//	while (true) {
+//		System.out.print("Key를 입력해주세요>> ");
+//		//String input_k = sc.nextLine();
+//		System.out.print("Value를 입력해주세요");
+//		//String input_v = sc.nextLine();
+//		//map.put(input_k, input_v);
+//		System.out.println("계속 입력하시겠습니까? 1.예 2.아니오");
+//		//int onoff = sc.nextInt();
+//		//if(onoff < 0) {
+//			System.out.println("입력을 종료합니다...");
+//			//flag = false;
+//			break;
+//		}
+//	}
+//
+//}
