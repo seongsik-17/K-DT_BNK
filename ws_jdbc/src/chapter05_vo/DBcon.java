21bddd0e08bc92fed08854e20cac5a0f1e59e5c1
seongsik-17 <mss9810@naver.com>
2025-04-17 23:46:45 +0900

    set on macbook

--- ws_jdbc/src/chapter05_vo/DBcon.java
+++ ws_jdbc/src/chapter05_vo/DBcon.java
@@ -10,8 +10,8 @@ public class DBcon {
 	
 	public Connection connect() throws ClassNotFoundException, SQLException{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		String url = "jdbc:oracle:thin:@localhost:1521:testdb";
-		String id = "green";
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String id = "scott";
 		String pw = "1234";
 		
 		Connection conn = DriverManager.getConnection(url,id,pw);
